1da95f8e5983ee6de826e4fa55a2081cef3a3e8b
Nikita Kalentev <nicknamewest@yandex.ru>
2024-09-01 16:57:38 +0300

    review1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/NikitaKalentev/mesto-project-ff
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,23 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+const placeElement = cardTemplate.querySelector(".places__item");
 
-// @todo: DOM узлы
+function makeCard(cardData, deleteButtonCallback) {
+  const placeElementClone = placeElement.cloneNode(true);
+  placeElementClone.querySelector(".card__title").textContent = cardData.name;
+  cardImage = placeElementClone.querySelector(".card__image");
+  cardImage.src = cardData.link;
+  cardImage.alt = "Пейзажное фото места " + cardData.name;
+  placeElementClone
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteButtonCallback);
+  return placeElementClone;
+}
 
-// @todo: Функция создания карточки
+function deleteCard(ev) {
+  ev.target.closest(".places__item").remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  placesList.append(makeCard(item, deleteCard));
+});
